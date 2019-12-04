04a4b18c168f0503162aaad19825403e6bffa212
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2019-12-04 16:42:18 -0300

    4

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,39 +1,11 @@
 package main
 
 import (
-	"io"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
 	"github.com/gustavosbarreto/revwebsocketdial/pkg/reverse"
 )
-
-var (
-	upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		Subprotocols:    []string{"binary"},
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-)
-
-const ProxiedFromDeviceHeader = "proxied-from-device"
-
-func ProxyResponseFromDevice(w http.ResponseWriter, resp *http.Response) {
-	for key, values := range resp.Header {
-		for _, value := range values {
-			w.Header().Add(key, value)
-		}
-	}
-	w.Header().Set(ProxiedFromDeviceHeader, "")
-
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
-	resp.Body.Close()
-}
 
 func main() {
 	rev := reverse.NewReverse(reverse.DefaultConnectionURL, reverse.DefaultRevdialURL)
